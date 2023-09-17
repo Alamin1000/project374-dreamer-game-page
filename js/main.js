0094d1366527dbf8fe4b2c3db6dcf88810ed0909
alamin <alaminhosen44000@gmail.com>
2023-09-17 20:43:13 +0600

    1.00

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
     $(".offcanvas-menu").addClass("active");
     $(".offcanvas-overlay").addClass("active");
   });
-  $(".offcanvas-menu a").click(function () {
+  $(".offcanvas-menu a:not(.for-button)").click(function () {
     $(".offcanvas-menu").removeClass("active");
     $(".offcanvas-overlay").removeClass("active");
   });
